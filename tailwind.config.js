5710537a8384c6f5b721cd6b4467c71b4f185352
Matias Sallent <mqsallent@gmail.com>
2022-11-29 22:00:55 -0300

    Extend theme with new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-black': '#141414',
+        'primary-blue': '#0E57FF',
+        'secondary-black': '#101010',
+        'secondary-blue': '#003BC3',
+        'secondary-white': '#F5F5F5',
+      },
+    },
+  },
 };
