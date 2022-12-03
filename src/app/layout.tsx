8028a15e96a35db29c23237af6dff6d0c1762d8a
Matias Sallent <mqsallent@gmail.com>
2022-12-03 03:16:50 -0300

    Change selected: color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <head />
-      <body className="bg-primary-black overscroll-none">
+      <body className="overscroll-none bg-primary-black selection:bg-secondary-white">
         {children}
         <div className="fixed left-0 top-0 mix-blend-overlay bg-grain-texture w-full h-full pointer-events-none" />
       </body>
