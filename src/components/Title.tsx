2194b1da9805c6cf3db28dba36d4e20e8d384e15
Matias Sallent <mqsallent@gmail.com>
2022-12-03 14:31:37 -0300

    Use font-title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ type TitleProps = {
 
 export function Title({ value }: TitleProps) {
   return (
-    <h1 className="text-[200px] leading-[78.5%] tracking-[-0.02em] [text-shadow:0_0_19px_rgba(0,59,195,0.4)] text-primary-blue">
+    <h1 className="font-title text-[200px] leading-[78.5%] tracking-[-0.02em] [text-shadow:0_0_19px_rgba(0,59,195,0.4)] text-primary-blue">
       {value}
     </h1>
   );
