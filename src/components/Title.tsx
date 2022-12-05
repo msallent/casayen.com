1497c2d5a9ac4669bb99eca659a708f376bb3bdc
Matias Sallent <mqsallent@gmail.com>
2022-12-05 15:02:13 -0300

    Extract styles into custom class

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,9 +5,5 @@ type TitleProps = {
 };
 
 export function Title({ children }: TitleProps) {
-  return (
-    <h1 className="font-title text-[200px] leading-[78.5%] tracking-[-0.02em] [text-shadow:0_0_19px_rgba(0,59,195,0.4)] text-primary-blue">
-      {children}
-    </h1>
-  );
+  return <h1 className="font-title-full text-[200px] text-primary-blue">{children}</h1>;
 }
