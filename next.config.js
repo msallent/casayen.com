f755ee70b4d3ac7df3f6b07abb56f752bbfce441
Matias Sallent <mqsallent@gmail.com>
2022-12-05 14:38:10 -0300

    Add svgr loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.tsx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
