b09fea7d8405ef63ca4fc4fb72e6e61a86fd8c75
Matias Sallent <mqsallent@gmail.com>
2022-12-05 15:55:41 -0300

    Add higher z-index to grain background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function Layout({ children }: LayoutProps) {
       <head />
       <body className="overscroll-none bg-primary-black selection:bg-secondary-white">
         {children}
-        <div className="fixed left-0 top-0 mix-blend-overlay bg-grain-texture w-full h-full pointer-events-none" />
+        <div className="fixed left-0 top-0 z-10 mix-blend-overlay bg-grain-texture w-full h-full pointer-events-none" />
       </body>
     </html>
   );
