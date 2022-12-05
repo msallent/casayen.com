d6f92f922e03c2834daa08c4ed68358735167428
Matias Sallent <mqsallent@gmail.com>
2022-12-05 15:05:34 -0300

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import classNames from 'classnames';
+
+type ButtonVariant = 'primary' | 'secondary';
+
+type ButtonProps = {
+  children: ReactNode;
+  variant?: ButtonVariant;
+  onClick?: () => void;
+};
+
+export function Button({ children, variant = 'primary', onClick }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={classNames(
+        'w-full h-16 rounded-sm font-serif font-medium uppercase tracking-[0.075em] [text-shadow:0_0_7px_rgba(255,255,255,0.65)] text-secondary-white',
+        { 'bg-primary-blue': variant === 'primary' },
+        { 'bg-primary-black': variant === 'secondary' }
+      )}
+    >
+      {children}
+    </button>
+  );
+}
