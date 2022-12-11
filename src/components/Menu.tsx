99a99f56d330fd21537aa1b1e49327292f18df20
Matias Sallent <mqsallent@gmail.com>
2022-12-11 03:51:00 -0300

    Increase z-index

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,7 +14,7 @@ const MENU_LINKS = [
   { label: 'Blog', path: '/' },
   { label: 'FAQ', path: '/' },
   { label: 'Contacto', path: '/' },
-];
+] as const;
 
 type MenuProps = {
   onClose: () => void;
@@ -22,7 +22,7 @@ type MenuProps = {
 
 export function Menu({ onClose }: MenuProps) {
   return createPortal(
-    <div className="fixed inset-0 px-5 py-[30px] overflow-auto bg-primary-blue">
+    <div className="fixed inset-0 px-5 py-[30px] overflow-auto z-20 bg-primary-blue">
       <div className="flex items-center justify-between">
         <YenSVG className="h-8" />
         <button type="button" aria-label="Cerrar menu" onClick={onClose}>
