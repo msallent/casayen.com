df2443c7497f80d6b865603a0531c8cc1e1e5026
Matias Sallent <mqsallent@gmail.com>
2022-12-11 17:17:38 -0300

    Add home/Shop component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from '@/components/home/About';
 import { Hero } from '@/components/home/Hero';
 import { Links } from '@/components/home/Links';
+import { Shop } from '@/components/home/Shop';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Links />
+      <Shop />
     </>
   );
 }
---
+++ src/components/home/Shop.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/Button';
+import { Title } from '@/components/Title';
+import GlyphsSVG from '@/assets/svgs/glyphs.svg';
+import RunesSVG from '@/assets/svgs/runes.svg';
+import ShopGradientSVG from '@/assets/svgs/shop-gradient.svg';
+
+export function Shop() {
+  return (
+    <section className="mt-40 pb-20 relative">
+      <RunesSVG className="absolute top-[-45px] mx-5" />
+
+      <div className="relative pt-40 mx-5 text-center flex flex-col items-center">
+        <GlyphsSVG className="w-28 mb-8" />
+        <Title variant="secondary" size="small">
+          Productos YEN
+        </Title>
+        <div className="mt-4 mb-11 w-[330px] uppercase text-sm leading-[18px]">
+          Encontrá en un solo lugar todas las herramientas que necesitás para embarcarte en tu viaje
+          de auto-descubrimiento y reprogramación sexual. productos ética y responsablemente
+          producidos.
+        </div>
+        <Button variant="secondary">
+          <a
+            className="flex h-full items-center justify-center"
+            href="https://shop.casayen.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            IR AL SHOP ONLINE →
+          </a>
+        </Button>
+      </div>
+
+      <ShopGradientSVG className="absolute top-0 -left-0 -z-10" />
+    </section>
+  );
+}
