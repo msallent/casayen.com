533dc146ed5d2560db580e580fd4df317003fba8
Matias Sallent <mqsallent@gmail.com>
2022-12-11 03:51:38 -0300

    Add small variant

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,23 @@
 import { ReactNode } from 'react';
+import classNames from 'classnames';
+
+type TitleSize = 'small' | 'default';
 
 type TitleProps = {
+  size?: TitleSize;
   children: ReactNode;
 };
 
-export function Title({ children }: TitleProps) {
-  return <h1 className="font-title-full text-[200px] text-primary-blue">{children}</h1>;
+export function Title({ size = 'default', children }: TitleProps) {
+  return (
+    <h1
+      className={classNames(
+        'font-title-full text-primary-blue',
+        { 'text-[200px]': size === 'default' },
+        { 'text-[60px]': size === 'small' }
+      )}
+    >
+      {children}
+    </h1>
+  );
 }
