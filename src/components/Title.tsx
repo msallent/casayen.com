6fecac0fa9cd76b5dfbeba102b1f732d3bbfea1a
Matias Sallent <mqsallent@gmail.com>
2023-01-14 18:39:58 -0300

    Re-add size prop

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,18 +1,22 @@
 import { ReactNode } from 'react';
 import classNames from 'classnames';
 
+type TitleSize = 'small' | 'default';
 type TitleVariant = 'primary' | 'secondary';
 
 type TitleProps = {
+  size?: TitleSize;
   variant?: TitleVariant;
   children: ReactNode;
 };
 
-export function Title({ variant = 'primary', children }: TitleProps) {
+export function Title({ size = 'default', variant = 'primary', children }: TitleProps) {
   return (
     <h1
       className={classNames(
-        'font-title-full text-[60px] text-primary-blue',
+        'font-title-full text-primary-blue',
+        { 'text-[60px]': size === 'default' },
+        { 'text-[40px]': size === 'small' },
         { 'text-primary-blue': variant === 'primary' },
         { 'text-primary-black': variant === 'secondary' }
       )}
