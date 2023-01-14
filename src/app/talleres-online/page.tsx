b56514540d5210760394db8376f1effd3af7a8ed
Matias Sallent <mqsallent@gmail.com>
2023-01-14 20:14:39 -0300

    Reduce margin-top

--- src/app/talleres-online/page.tsx
+++ src/app/talleres-online/page.tsx
@@ -3,14 +3,16 @@ import { TitlePage } from '@/components/TitlePage';
 
 export default function Talleres() {
   return (
-    <section className="mt-10 mx-5">
+    <section className="mt-4 mx-5">
       <div className="mx-5 mb-4 text-center">
         <TitlePage>TALLERES ONLINE</TitlePage>
       </div>
+
       <div className="uppercase text-center text-primary-blue">
         En esta sección vas a encontrar TODOS los talleres pregrabados para que hagas a tu propio
         ritmo.
       </div>
+
       <List />
     </section>
   );
