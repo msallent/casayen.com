1365213db1c1df37a4a164d0624771a0d98ac074
Matias Sallent <mqsallent@gmail.com>
2023-01-14 18:52:33 -0300

    Simplify page's title

--- src/app/talleres-online/page.tsx
+++ src/app/talleres-online/page.tsx
@@ -1,13 +1,11 @@
 import { List } from '@/components/talleres-online/List';
-import { Title } from '@/components/Title';
 import { TitlePage } from '@/components/TitlePage';
 
 export default function Talleres() {
   return (
     <section className="mt-10 mx-5">
-      <TitlePage>TALLERES</TitlePage>
-      <div className="flex justify-center mt-px mb-4">
-        <Title>ONLINE</Title>
+      <div className="mx-5 mb-4 text-center">
+        <TitlePage>TALLERES ONLINE</TitlePage>
       </div>
       <div className="uppercase text-center text-primary-blue">
         En esta sección vas a encontrar TODOS los talleres pregrabados para que hagas a tu propio
