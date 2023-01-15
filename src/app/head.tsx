bc18a941d27f4d43ff1e2e5ac2de730f287698b0
Matias Sallent <mqsallent@gmail.com>
2023-01-15 18:14:24 -0300

    Add meta description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>casayen.com</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta content="Casa Yen" name="description" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
