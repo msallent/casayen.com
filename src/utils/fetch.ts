bccd32e798f78d19ac982ad55decc68262cd3f1c
Matias Sallent <mqsallent@gmail.com>
2023-01-16 12:12:29 -0300

    Refactor fetch to use GraphQL

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,10 +1,18 @@
 import { RequestInit } from 'next/dist/server/web/spec-extension/request';
 import { ContentfulResponse } from '@/types/contentful';
 
-export async function fetchContent<T = any>(input: string, init?: RequestInit) {
+export async function fetchContent<T = any>(query: string, init?: RequestInit) {
   const response = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/entries?content_type=${input}`,
-    { headers: { Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}` }, ...init }
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+      ...init,
+    }
   );
 
   return response.json() as Promise<ContentfulResponse<T>>;
