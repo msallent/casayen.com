5d02965738b505bf16181e4c89fe8cddabf63a18
Matias Sallent <mqsallent@gmail.com>
2023-01-28 16:36:48 -0300

    Fetch more data from Contentful

--- src/app/talleres-online/page.tsx
+++ src/app/talleres-online/page.tsx
@@ -6,8 +6,15 @@ import { PageTalleresOnlineData } from '@/types/contentful';
 import backgroundGradient from '@/assets/images/gradient-1.webp';
 
 export default async function Talleres() {
-  const { data } = await fetchContent<PageTalleresOnlineData>(`query PageTalleresOnline {
+  const {
+    data: {
+      pageTalleresOnline: { title, subtitle, disclaimer, talleresCollection },
+    },
+  } = await fetchContent<PageTalleresOnlineData>(`query PageTalleresOnline {
     pageTalleresOnline(id: "${process.env.CONTENTFUL_PAGE_TALLERES_ONLINE_ID}") {
+      title
+      subtitle
+      disclaimer
       talleresCollection {
         items {
           hot
@@ -35,24 +42,16 @@ export default async function Talleres() {
     }
   }`);
 
-  const talleres = data.pageTalleresOnline.talleresCollection.items;
-
   return (
     <section className="mx-5 mt-4">
-      <TitlePage>TALLERES ONLINE</TitlePage>
+      <TitlePage title={title} subtitle={subtitle} />
 
-      <div className="mt-4 text-center uppercase text-primary-blue">
-        En esta sección vas a encontrar TODOS los talleres pregrabados para que hagas a tu propio
-        ritmo.
-      </div>
-
-      <div className="mt-12 font-serif uppercase italic text-primary-blue">
-        (!) Importante: los videos estarán disponibles por 10 días (una vez que envíes tu
-        comprobante de pago)
-      </div>
+      {disclaimer && (
+        <div className="mt-12 font-serif uppercase italic text-primary-blue">{disclaimer}</div>
+      )}
 
       <div className="mt-5 mb-20">
-        {talleres.map((taller) => (
+        {talleresCollection.items.map((taller) => (
           <ExpandableTaller key={taller.title} taller={taller} />
         ))}
       </div>
