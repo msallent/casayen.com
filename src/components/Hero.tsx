1c5b1684c28e8ed966e6c42aceb7bfa8d5c3b5f4
Matias Sallent <mqsallent@gmail.com>
2023-01-30 15:58:24 -0300

    Add support for medium screens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,16 +8,16 @@ import ChevronDouble from '@/assets/svgs/chevron-double.svg';
 export function Hero() {
   return (
     <div className="font-title-full relative mx-5 mb-14 flex flex-col text-[24vw] sm:mb-20 sm:text-[16vw]">
-      <Stars2SVG className="hidden sm:mx-auto sm:mb-20 sm:block sm:w-3/5" />
+      <Stars2SVG className="hidden sm:mx-auto sm:mb-20 sm:block sm:w-3/5 md:mb-24" />
 
-      <div className="relative ml-[6%] flex items-center gap-1.5 sm:ml-[22%]">
+      <div className="relative ml-[6%] flex items-center gap-1.5 sm:ml-[22%] md:ml-[27%]">
         <div className="text-[4.5rem]">→</div>
         <div className="w-20 text-right font-sans text-xs tracking-normal sm:-mt-2.5 sm:ml-2 sm:mr-4 sm:text-sm sm:leading-[117%]">
           BIENVENIDX A TU
         </div>
         <h1>VIAJE</h1>
         <EllipsisSVG className="pointer-events-none absolute top-[-30%] left-[4%] z-10 w-[16.5rem]" />
-        <Stars1SVG className="pointer-events-none absolute top-[-60%] left-[87%] z-10 w-12 sm:left-[75%]" />
+        <Stars1SVG className="pointer-events-none absolute top-[-60%] left-[87%] z-10 w-12 sm:left-[75%] md:left-[73%] md:w-14" />
       </div>
 
       <div className="sm:hidden">
@@ -34,17 +34,19 @@ export function Hero() {
       <div className="hidden sm:block sm:text-center">
         <h1>INTERGALÁCTICO DE</h1>
         <h1>REPROGRAMACION</h1>
-        <h1 className="relative">
+        <h1 className="sm:relative">
           <span>SEXUAL</span>
-          <div className="absolute top-[-54%] right-[28%] rotate-90 text-[3rem]">‡</div>
+          <div className="sm:absolute sm:top-[-54%] sm:right-[28%] sm:rotate-90 sm:text-[3rem]">
+            ‡
+          </div>
         </h1>
       </div>
 
       <Stars3SVG className="hidden sm:mx-auto sm:mt-6 sm:block sm:w-3/5" />
 
       <div className="mt-6 flex items-center justify-between sm:mx-5 sm:mt-12">
-        <YenSloganSVG className="hidden w-36 sm:block" />
-        <ChevronDouble className="mx-auto w-8 sm:mx-0 sm:w-6" />
+        <YenSloganSVG className="hidden sm:block sm:w-36 md:w-40" />
+        <ChevronDouble className="mx-auto w-8 sm:mx-0 sm:w-6 md:w-8" />
       </div>
     </div>
   );
