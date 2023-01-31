0790decdcc14b6d552309eda4bb463f0fce61581
Matias Sallent <mqsallent@gmail.com>
2023-01-30 23:55:43 -0300

    Use Balancer from subtitle

--- src/components/TitlePage.tsx
+++ src/components/TitlePage.tsx
@@ -1,3 +1,4 @@
+import Balancer from 'react-wrap-balancer';
 import { Title } from '@/components/Title';
 import StarsSVG from '@/assets/svgs/stars-2.svg';
 
@@ -11,7 +12,11 @@ export function TitlePage({ title, subtitle }: TitlePageProps) {
     <div className="flex flex-col items-center justify-center text-center uppercase text-primary-blue">
       <StarsSVG className="mx-auto mt-2 mb-4 h-6 w-6" />
       <Title>{title}</Title>
-      {subtitle && <div className="mt-4">{subtitle}</div>}
+      {subtitle && (
+        <div className="mt-4">
+          <Balancer>{subtitle}</Balancer>
+        </div>
+      )}
     </div>
   );
 }
