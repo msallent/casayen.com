325a6b8a32a2435ad1a89137d4ad001c1e04380b
Matias Sallent <mqsallent@gmail.com>
2023-01-31 23:22:25 -0300

    Fix title's positioning

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,13 +10,13 @@ export function Hero() {
     <div className="font-title-full relative mx-5 mb-14 flex flex-col text-[24vw] sm:mb-20 sm:text-[16vw]">
       <Stars2SVG className="hidden sm:mx-auto sm:block sm:w-3/5 lg:hidden" />
 
-      <div className="relative ml-[6%] flex items-center gap-1.5 sm:mt-20 sm:ml-[22%] md:mt-24 md:ml-[27%] lg:ml-[18%]">
+      <div className="relative ml-[6%] flex items-center gap-1.5 sm:mt-20 sm:ml-[22%] md:mt-24 md:ml-[27%] lg:ml-[8%]">
         <div className="text-[4.5rem] lg:text-[10rem]">→</div>
         <div className="w-20 text-right font-sans text-xs tracking-normal sm:-mt-2.5 sm:ml-2 sm:mr-4 sm:text-sm sm:leading-[117%] lg:ml-6 lg:-mt-8 lg:w-32 lg:text-xl">
           BIENVENIDX A TU
         </div>
         <h1>VIAJE</h1>
-        <EllipsisSVG className="pointer-events-none absolute top-[-30%] left-[4%] z-10 w-[16.5rem] lg:top-[-50%] lg:left-[5%] lg:w-[30rem]" />
+        <EllipsisSVG className="pointer-events-none absolute top-[-30%] left-[4%] z-10 w-[16.5rem] lg:top-[-55%] lg:left-[9%] lg:w-[26rem]" />
         <Stars1SVG className="pointer-events-none absolute top-[-60%] left-[87%] z-10 w-12 sm:left-[75%] md:left-[73%] md:w-14 lg:left-[74%] lg:top-[-68%] lg:w-24" />
       </div>
 
@@ -44,7 +44,7 @@ export function Hero() {
 
       <Stars3SVG className="hidden sm:mx-auto sm:mt-6 sm:block sm:w-3/5 lg:hidden" />
 
-      <div className="mt-6 flex items-center justify-between sm:mx-5 sm:mt-12">
+      <div className="mt-6 flex items-center justify-between sm:mx-5 sm:mt-12 lg:mx-14">
         <YenSloganSVG className="hidden sm:block sm:w-36 md:w-40" />
         <ChevronDouble className="mx-auto w-8 sm:mx-0 sm:w-6 md:w-8" />
       </div>
