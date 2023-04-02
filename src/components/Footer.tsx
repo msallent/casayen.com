5f938cc1abca0422e921b69b8f0ceb1e75d48473
Matias Sallent <mqsallent@gmail.com>
2023-04-02 20:11:36 -0300

    Add support for xlarge devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,33 +4,36 @@ import InstagramSVG from '@/assets/svgs/instagram.svg';
 
 export function Footer() {
   return (
-    <footer className="bg-secondary-black px-5 py-8 sm:p-10 lg:px-20">
-      <div className="border-b border-primary-blue pb-5">
-        <YenSloganSVG className="w-40" />
-      </div>
+    <footer className="bg-secondary-black px-5 py-8 sm:p-10 lg:px-20 xl:px-10 xl:py-14">
+      <div className="relative xl:flex xl:justify-between">
+        <div className="border-b border-primary-blue pb-5 xl:basis-1/2 xl:self-start">
+          <YenSloganSVG className="w-40" />
+        </div>
 
-      <div className="relative flex flex-col text-primary-blue">
-        <span className="mb-2 mt-10 uppercase">Contacto Yen</span>
-        <a href="mailto:info@casayen.com">
-          <Title size="small">info@casayen.com</Title>
-        </a>
+        <div className="flex flex-col text-primary-blue">
+          <span className="mb-2 mt-10 uppercase xl:mt-0">Contacto Yen</span>
+          <a href="mailto:info@casayen.com">
+            <Title size="small">info@casayen.com</Title>
+          </a>
+          <span className="mb-2 mt-8 uppercase">Contacto Shop</span>
+          <a href="mailto:shop@casayen.com">
+            <Title size="small">shop@casayen.com</Title>
+          </a>
+        </div>
 
-        <span className="mb-2 mt-8 uppercase">Contacto Shop</span>
-        <a href="mailto:shop@casayen.com">
-          <Title size="small">shop@casayen.com</Title>
-        </a>
+        <div className="flex flex-col text-primary-blue">
+          <span className="mb-2 mt-8 uppercase xl:mt-0">Social</span>
+          <a
+            aria-label="Instagram"
+            href="https://instagram.com/casa.yen"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <InstagramSVG className="w-8" />
+          </a>
+        </div>
 
-        <span className="mb-2 mt-8 uppercase">Social</span>
-        <a
-          aria-label="Instagram"
-          href="https://instagram.com/casa.yen"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <InstagramSVG className="w-8" />
-        </a>
-
-        <span className="absolute bottom-0 right-0 text-[1.14rem] underline">
+        <span className="absolute bottom-0 right-0 text-[1.14rem] text-primary-blue underline xl:left-0 xl:text-base">
           TÉRMINOS Y CONDICIONES
         </span>
       </div>
