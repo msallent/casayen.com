14008241e4a245de189d93855a34485aec04380c
Matias Sallent <mqsallent@gmail.com>
2023-04-02 19:32:42 -0300

    Change to use absolute position

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,30 +7,30 @@ export function Footer() {
     <footer className="bg-secondary-black px-5 py-8 sm:p-10 lg:px-20">
       <YenSloganSVG className="w-36" />
 
-      <div className="mt-5 flex flex-col border-t border-primary-blue text-primary-blue">
-        <span className="mt-10 mb-2 uppercase">Contacto Yen</span>
+      <div className="relative mt-5 flex flex-col border-t border-primary-blue text-primary-blue">
+        <span className="mb-2 mt-10 uppercase">Contacto Yen</span>
         <a href="mailto:info@casayen.com">
           <Title size="small">info@casayen.com</Title>
         </a>
 
-        <span className="mt-8 mb-2 uppercase">Contacto Shop</span>
+        <span className="mb-2 mt-8 uppercase">Contacto Shop</span>
         <a href="mailto:shop@casayen.com">
           <Title size="small">shop@casayen.com</Title>
         </a>
 
-        <span className="mt-8 mb-2 uppercase">Social</span>
-        <div className="flex items-end justify-between">
-          <a
-            aria-label="Instagram"
-            href="https://instagram.com/casa.yen"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <InstagramSVG className="w-8" />
-          </a>
+        <span className="mb-2 mt-8 uppercase">Social</span>
+        <a
+          aria-label="Instagram"
+          href="https://instagram.com/casa.yen"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <InstagramSVG className="w-8" />
+        </a>
 
-          <span className="text-[1.14rem] underline">TÉRMINOS Y CONDICIONES</span>
-        </div>
+        <span className="absolute bottom-0 right-0 text-[1.14rem] underline">
+          TÉRMINOS Y CONDICIONES
+        </span>
       </div>
     </footer>
   );
