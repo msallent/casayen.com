1cab12bd6c78fb52423dac967c650f6d3d4abe96
Matias Sallent <mqsallent@gmail.com>
2023-04-03 02:25:15 -0300

    Make Button less tall on larger devices

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ export function Button({ children, variant = 'primary', onClick }: ButtonProps) 
       type="button"
       onClick={onClick}
       className={classNames(
-        'w-full h-16 rounded-sm font-serif font-medium uppercase tracking-[0.075em] [text-shadow:0_0_7px_rgba(255,255,255,0.65)] text-secondary-white',
+        'w-full h-16 rounded-sm font-serif font-medium uppercase tracking-[0.075em] [text-shadow:0_0_7px_rgba(255,255,255,0.65)] text-secondary-white xl:h-12',
         { 'bg-primary-blue': variant === 'primary' },
         { 'bg-primary-black': variant === 'secondary' }
       )}
