d200e6ca19e333bc65954ffbc82bb167030718c9
Matias Sallent <mqsallent@gmail.com>
2023-08-05 20:55:34 -0300

    Add support for xl devices

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -4,7 +4,8 @@ import { Expandable } from '@/components/Expandable';
 import { parseRichText } from '@/utils/richText';
 import { fetchContent } from '@/utils/fetch';
 import { PageFAQData } from '@/types/contentful';
-import backgroundGradient from '@/assets/images/gradient-1.webp';
+import backgroundGradient1 from '@/assets/images/gradient-1.webp';
+import backgroundGradient2 from '@/assets/images/gradient-4.webp';
 
 export default async function FAQ() {
   const {
@@ -27,22 +28,31 @@ export default async function FAQ() {
   }`);
 
   return (
-    <section className="mx-5 sm:mx-10 lg:mx-20">
-      <TitlePage title={title} subtitle={subtitle} />
-
-      <div className="mb-20 mt-12 border-b border-primary-blue">
-        {faqsCollection.items.map((faq) => (
-          <Expandable key={faq.title} title={faq.title}>
-            <div className="text-primary-blue">{parseRichText(faq.content.json)}</div>
-          </Expandable>
-        ))}
+    <section className="mx-5 sm:mx-10 lg:mx-20 xl:relative xl:mt-28">
+      <div className="xl:flex xl:items-start xl:gap-28">
+        <TitlePage title={title} subtitle={subtitle} vertical />
+        <div className="mb-20 mt-12 border-b border-primary-blue xl:mt-0">
+          {faqsCollection.items.map((faq, index) => (
+            <Expandable key={faq.title} title={faq.title} noBorder={index === 0}>
+              <div className="text-primary-blue">{parseRichText(faq.content.json)}</div>
+            </Expandable>
+          ))}
+        </div>
       </div>
 
       <Image
         aria-hidden
-        src={backgroundGradient}
+        src={backgroundGradient1}
         alt="Background gradient"
-        className="absolute left-0 top-0 -z-10 max-h-[1200px] w-full"
+        className="absolute left-0 top-0 -z-10 max-h-[1200px] w-full xl:hidden"
+        placeholder="blur"
+      />
+
+      <Image
+        aria-hidden
+        src={backgroundGradient2}
+        alt="Background gradient"
+        className="hidden xl:absolute xl:-bottom-48 xl:-left-20 xl:-z-10 xl:block xl:w-2/3"
         placeholder="blur"
       />
     </section>
