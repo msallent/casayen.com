b46ef8466347b553c1b23ac05f156231f6351cd9
Matias Sallent <mqsallent@gmail.com>
2023-08-06 00:58:56 -0300

    Switch anchor and div

--- src/components/LinkFeatured.tsx
+++ src/components/LinkFeatured.tsx
@@ -12,15 +12,13 @@ type LinkProps = {
 
 export function LinkFeatured({ href, index, children, isLast }: LinkProps) {
   return (
-    <NextLink
-      href={href}
-      className="font-title-full relative flex justify-between text-[2.85rem] uppercase leading-[78.5%] hover:text-secondary-white xl:text-[3.75rem]"
-    >
+    <div className="font-title-full relative flex justify-between text-[2.85rem] uppercase leading-[78.5%] xl:text-[3.75rem] 2xl:text-[5rem]">
       <SnowflakeSVG className="absolute left-0 top-1/2 w-2 -translate-y-1/2 xl:w-5" />
 
-      <div
+      <NextLink
+        href={href}
         className={classNames(
-          'relative w-full border-t border-primary-blue pt-8 pb-4 text-center xl:mx-auto xl:w-[45rem] xl:pt-10 xl:pb-6',
+          'relative w-full border-t border-primary-blue pt-8 pb-4 text-center hover:text-secondary-white xl:mx-auto xl:w-[45rem] xl:pt-10 xl:pb-6',
           { 'border-b': isLast }
         )}
       >
@@ -28,9 +26,9 @@ export function LinkFeatured({ href, index, children, isLast }: LinkProps) {
           {index.toString().padStart(2, '0')}
         </span>
         <span className="ml-5">{children} →</span>
-      </div>
+      </NextLink>
 
       <SnowflakeSVG className="absolute right-0 top-1/2 w-2 -translate-y-1/2 xl:w-5" />
-    </NextLink>
+    </div>
   );
 }
