3af8207b58647ce3be912fdd6269ff84f67b2199
Matias Sallent <mqsallent@gmail.com>
2023-08-07 13:39:10 -0300

    Add PageEvent component

--- src/app/presenciales/page.tsx
+++ src/app/presenciales/page.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image';
-import { ExpandableEvent } from '@/components/ExpandableEvent';
-import { TitlePage } from '@/components/TitlePage';
-import { fetchContent } from '@/utils/fetch';
+import { PageEvent } from '@/components/PageEvent';
 import { PagePresencialesData } from '@/types/contentful';
-import backgroundGradient from '@/assets/images/gradient-1.webp';
+import { fetchContent } from '@/utils/fetch';
 
 export default async function Presenciales() {
   const {
@@ -43,28 +40,12 @@ export default async function Presenciales() {
   }`);
 
   return (
-    <section className="mx-5 sm:mx-10 lg:mx-20 2xl:mx-48">
-      <TitlePage title={title} subtitle={subtitle} />
-
-      {disclaimer && (
-        <div className="mt-12 font-serif uppercase italic text-primary-blue xl:mt-24">
-          {disclaimer}
-        </div>
-      )}
-
-      <div className="mb-20 mt-5 border-b border-primary-blue">
-        {talleresCollection.items.map((taller, index) => (
-          <ExpandableEvent key={taller.title} type="taller" event={taller} index={index + 1} />
-        ))}
-      </div>
-
-      <Image
-        aria-hidden
-        src={backgroundGradient}
-        alt="Background gradient"
-        className="absolute left-0 top-0 -z-10 max-h-[1200px] w-full"
-        placeholder="blur"
-      />
-    </section>
+    <PageEvent
+      type="taller"
+      title={title}
+      subtitle={subtitle}
+      disclaimer={disclaimer}
+      events={talleresCollection.items}
+    />
   );
 }
---
+++ src/components/PageEvent.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import { TitlePage } from '@/components/TitlePage';
+import { ExpandableEvent } from '@/components/ExpandableEvent';
+import { Event } from '@/types/contentful';
+import backgroundGradient from '@/assets/images/gradient-1.webp';
+
+type EventType = 'programa' | 'taller';
+
+type PageEvent = {
+  title: string;
+  type: EventType;
+  subtitle?: string;
+  disclaimer?: string;
+  events: Array<Event>;
+};
+
+export function PageEvent({ title, subtitle, disclaimer, events, type }: PageEvent) {
+  return (
+    <section className="mx-5 sm:mx-10 lg:mx-20 2xl:mx-48">
+      <TitlePage title={title} subtitle={subtitle} />
+
+      {disclaimer && (
+        <div className="mt-12 font-serif uppercase italic text-primary-blue xl:mt-24">
+          {disclaimer}
+        </div>
+      )}
+
+      <div className="mb-20 mt-5 border-b border-primary-blue">
+        {events.map((event, index) => (
+          <ExpandableEvent key={event.title} type={type} event={event} index={index + 1} />
+        ))}
+      </div>
+
+      <Image
+        aria-hidden
+        src={backgroundGradient}
+        alt="Background gradient"
+        className="absolute left-0 top-0 -z-10 max-h-[1200px] w-full"
+        placeholder="blur"
+      />
+    </section>
+  );
+}
