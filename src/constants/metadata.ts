5273088064a713b91eea90577951a0a56ab5b41c
Matias Sallent <mqsallent@gmail.com>
2023-08-14 17:19:02 -0300

    Add new OG image

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,4 +1,4 @@
 export const openGraph = {
   type: 'website',
-  images: [{ type: 'image/jpeg', width: 1200, height: 630, url: '/images/opengraph-image.jpg' }],
+  images: [{ type: 'image/jpeg', width: 1200, height: 630, url: '/images/yen-1200x630.jpg' }],
 };
